7462ec7d22b974ec98aa7034e943d8db2f475185
KarenP <karenpadron0608@gmail.com>
2026-01-15 16:46:43 -0600

    Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Navbar from './componentes/Navbar'
+import Banner from "./componentes/Banner";
+
 const App = () => {
- return (
- <div className='overflow-x-hidden'>
- <Navbar />
- </div>
- )
+    return (
+        <div className='overflow-x-hidden'>
+            <Navbar />
+            <Banner />
+        </div>
+    )
 }
 export default App
