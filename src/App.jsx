fe18ef014cfeaceb86b13bbc9fd2d09dad71ccd3
Karen-Anahi-Padron-Martinez <karenpadron0608@gmail.com>
2026-01-22 18:46:40 -0600

    Barra de Navegacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import Navbar from './componentes/Navbar'
 import Banner from "./componentes/Banner";
+import Home from "./componentes/secciones/Home"
+import Cursos from "./componentes/secciones/Cursos"
+import About from "./componentes/secciones/About"
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 const App = () => {
     return (
         <div className='overflow-x-hidden'>
+            <BrowserRouter>
             <Navbar />
-            <Banner />
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/cursos" element={<Cursos/>}/>
+                <Route path="/about" element={<About/>}/>
+            </Routes>
+            
+            </BrowserRouter>
         </div>
     )
 }
